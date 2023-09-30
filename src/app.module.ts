00e690dd0ee37b337db946f3afb000f84e479daf
Tirth <tirthacodes@gmail.com>
2023-09-30 17:46:19 +0545

    modules generated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ShortenModule } from './shorten/shorten.module';
+import { ExpnadModule } from './expnad/expnad.module';
 
 @Module({
-  imports: [],
+  imports: [ShortenModule, ExpnadModule],
   controllers: [AppController],
   providers: [AppService],
 })
